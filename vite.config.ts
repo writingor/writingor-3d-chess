c58aa610dc6cd938123cffa513d8b1f42123511b
writingor <writingor.com@gmail.com>
2025-01-04 07:28:43 +0100

    setup paths build

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig({
-    plugins: [react()],
-    base: './',
-    build: {
-        outDir: 'docs'
-    },
-    assetsInclude: ['**/*.glb'],
-    resolve: {
-        alias: {
-            '@app': path.resolve(__dirname, './src/app'),
-            '@features': path.resolve(__dirname, './src/features'),
-            '@widgets': path.resolve(__dirname, './src/widgets'),
-            '@entities': path.resolve(__dirname, './src/entities'),
-            '@shared': path.resolve(__dirname, './src/shared')
+export default defineConfig(({ mode }) => {
+    return {
+        plugins: [react()],
+        base: mode === 'development' ? './' : '/writingor-3d-chess/',
+        build: {
+            outDir: 'docs'
+        },
+        assetsInclude: ['**/*.glb'],
+        resolve: {
+            alias: {
+                '@app': path.resolve(__dirname, './src/app'),
+                '@features': path.resolve(__dirname, './src/features'),
+                '@widgets': path.resolve(__dirname, './src/widgets'),
+                '@entities': path.resolve(__dirname, './src/entities'),
+                '@shared': path.resolve(__dirname, './src/shared')
+            }
         }
     }
 })
